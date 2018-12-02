a6aa12dc600bd52763c958901c0a3c534603de04
gotoengineering.io <max@gotoengineering.io>
2018-12-02 10:56:44 -0800

    Initial commit

---
+++ lib/lfex.ex
@@ -0,0 +1,18 @@
+defmodule Lfex do
+  @moduledoc """
+  Documentation for Lfex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lfex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lfex_test.exs
@@ -0,0 +1,8 @@
+defmodule LfexTest do
+  use ExUnit.Case
+  doctest Lfex
+
+  test "greets the world" do
+    assert Lfex.hello() == :world
+  end
+end
