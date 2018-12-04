86d0e7108382b251ccd27beee4b91947730ebcf1
gotoengineering.io <max@gotoengineering.io>
2018-12-03 22:10:15 -0800

    Fix repl

--- lib/lfex.ex
+++ lib/lfex.ex
@@ -2,19 +2,22 @@ defmodule Lfex do
   def interpret(program) do
     program
     |> Parse.parse
-    |> Eval.eval()
+    |> Eval.eval
   end
 
-  def to_lfex_string(exp) do
-    case is_list(exp) do
-      true -> "(" <> (Enum.map(exp,fn x -> to_lfex_string(x) end) |> Enum.join(" ")) <> ")"  
-      false -> to_string(exp)
-    end
+  def to_lfex_string(expr) when is_atom(expr), do: ":#{expr}"
+  def to_lfex_string(expr) when is_binary(expr), do: ~s("#{expr}")
+  def to_lfex_string(expr) when is_list(expr) do
+    string = expr
+             |> Enum.map(&to_lfex_string/1)
+             |> Enum.join(" ")
+    "[#{string}]"
   end
+  def to_lfex_string(expr), do: to_string(expr)
 
   def repl do
     program = IO.gets "lfex> "
-    {result}  = program |> interpret()
+    result  = program |> interpret()
     result |> to_lfex_string |> IO.puts
     repl()
   end
--- test/lfex_test.exs
+++ test/lfex_test.exs
@@ -1,3 +1,15 @@
 defmodule LfexTest do
   use ExUnit.Case
+
+  test "#interpret" do
+    program = """
+    (* 999 999)
+    """
+    assert Lfex.interpret(program) == 998001
+  end
+
+  test "#to_lfex_string" do
+    assert Lfex.to_lfex_string(123) == "123"
+    assert Lfex.to_lfex_string([1, :b, "Charlie"]) == ~s([1 :b "Charlie"])
+  end
 end
