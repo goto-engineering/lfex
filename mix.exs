244c7632a029d8bfe30db4a9b462e322fbf3092d
gotoengineering.io <max@gotoengineering.io>
2019-08-06 21:15:14 -0700

    Elixir version 1.7->1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Lfex.MixProject do
     [
       app: :lfex,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
